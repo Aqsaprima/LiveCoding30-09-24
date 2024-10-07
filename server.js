541bddacf72e7204f9d1c5bc90de1965bda86faf
Aqsaprima <aqsaprima11@gmail.com>
2024-10-07 22:12:32 +0700

    add connection to postgres database with ORM sequelize

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
 // const fsAsync = require("fs").promises;
 const express = require("express");
-
 const app = express();
-
+const { Product } = require("./models");
 // middleware untuk membaca json dari request body ke kita
 app.use(express.json());
 
@@ -176,6 +175,26 @@ app.delete("/api/v1/cars/:id", (req, res) => {
   );
 });
 
+//api get dengan fungsi async
+app.get("/api/v1/async", async (req, res) => {
+  try {
+    const cars = await Product.findAll();
+    res.status(200).json({
+      status: "success",
+      message: "success get data car async",
+      isSuccess: true,
+      data: cars,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "gagal",
+      message: error.message,
+      isSuccess: false,
+      data: null,
+    });
+  }
+});
+
 //middleware / handler utk url yang tidak dapat diakses karena tidak ada di aplikasi
 //membuat middleware
 app.use((req, res, next) => {
@@ -188,25 +207,3 @@ app.use((req, res, next) => {
 app.listen("3000", () => {
   console.log("start aplikasi di port 3000");
 });
-
-//api get dengan fungsi async
-/*app.get("/api/v1/async", async (req, res) => {
-  try {
-    let cars = JSON.parse(
-      await fsAsync.readFile(`${__dirname}/assets/data/cars.json`, "utf-8")
-    );
-    res.status(200).json({
-      status: "success",
-      message: "success get data car async",
-      isSuccess: true,
-      data: cars,
-    });
-  } catch (error) {
-    res.status(404).json({
-      status: "failed",
-      message: "failed get data car async",
-      isSuccess: false,
-      data: null,
-    });
-  }
-});*/
